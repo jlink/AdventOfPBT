0658696c962e403b56bf5849055694b3f054a63b
Johannes Link <business@johanneslink.net>
2021-12-05 11:49:08 +0100

    first solution attempt day 4

--- src/test/kotlin/adventOfPBT/day4/DetectCycleInLinkedListProperties.kt
+++ src/test/kotlin/adventOfPBT/day4/DetectCycleInLinkedListProperties.kt
@@ -1,30 +1,29 @@
 package adventOfPBT.day4
 
 import adventOfPBT.day4.LinkedListDomain.Cycles
-import adventOfPBT.day4.LinkedListDomain.CyclesMode.NO
+import adventOfPBT.day4.LinkedListDomain.CyclesMode.*
 import net.jqwik.api.*
-import net.jqwik.api.constraints.Size
 import net.jqwik.api.domains.Domain
 import net.jqwik.api.domains.DomainContextBase
 import net.jqwik.api.providers.TypeUsage
-import java.util.*
+import net.jqwik.kotlin.api.any
+import net.jqwik.kotlin.api.ofSize
 
 @Domain(LinkedListDomain::class)
 class DetectCycleInLinkedListProperties {
 
-    // check generation of linked lists
-
-    // no cycles in linked lists
-    @Property
+    @Property(tries = 10)
     fun noCycleDetectedInListsWithoutCycles(@ForAll @Cycles(NO) list: LinkedList) =
         detectCycleInLinkedList(list) == false
 
-    // cycles in linked lists
+    @Property(tries = 10)
+    fun cycleDetectedInListsWithCycles(@ForAll @Cycles(YES) list: LinkedList) =
+        detectCycleInLinkedList(list) == true
 }
 
 class LinkedListDomain : DomainContextBase() {
 
-    enum class CyclesMode { YES, NO, MAYBE }
+    enum class CyclesMode { YES, NO }
 
     @Retention(AnnotationRetention.RUNTIME)
     annotation class Cycles(val value: CyclesMode) {
@@ -32,12 +31,45 @@ class LinkedListDomain : DomainContextBase() {
 
     @Provide
     fun linkedLists(usage: TypeUsage): Arbitrary<LinkedList> {
-        val size: Optional<Size> = usage.findAnnotation(Size::class.java)
-        val cycles: Optional<Cycles> = usage.findAnnotation(Cycles::class.java)
-        if (cycles.isEmpty) {
+        val cycles = usage.findAnnotation(Cycles::class.java).map { it.value }.orElse(null)
+        if (cycles == null) {
             val message = "LinkedList generation requires annotation @Cycles"
             throw IllegalArgumentException(message)
         }
-        return Arbitraries.just(LinkedList(42))
+        return when(cycles) {
+            NO -> linkedListsWithoutCycles()
+            YES -> linkedListsWithCycles()
+        }
+    }
+
+    private fun linkedListsWithCycles(): Arbitrary<LinkedList> {
+        return linkedListsWithoutCycles().map {
+            it.last().next = it
+            it
+        }
+    }
+
+    private fun linkedListsWithoutCycles(): Arbitrary<LinkedList> {
+        val lists = Int.any().list().ofSize(1..100)
+        return lists.map { list ->
+            var current : LinkedList? = null
+            for (value in list) {
+                current = LinkedList(value, current)
+            }
+            current!!
+        }
+        // Recursion is involved:
+        // val sizes = Int.any(1..1000)
+        // return sizes.flatMap { size ->
+        //     Arbitraries.recursive(
+        //         { -> Int.any().map { LinkedList(it) } },
+        //         { a ->
+        //             a.flatMap { next ->
+        //                 Int.any().map { LinkedList(it, next) }
+        //             }
+        //         },
+        //         size
+        //     )
+        // }
     }
 }
--- src/test/kotlin/adventOfPBT/day4/LinkedList.kt
+++ src/test/kotlin/adventOfPBT/day4/LinkedList.kt
@@ -1,7 +1,31 @@
 package adventOfPBT.day4
 
-data class LinkedList(val value: Int, var next: LinkedList? = null)
+data class LinkedList(val value: Int, var next: LinkedList? = null) {
+    fun last(): LinkedList {
+        val visited = mutableListOf(this)
+        var current = this
+        while(current.next != null) {
+            if (current.next in visited) {
+                return current
+            }
+            current = current.next!!
+        }
+        return current
+    }
+
+    override fun toString(): String {
+        return "LinkedList(value=`$value`,hasNext=`${next != null}`)"
+    }
+}
 
 fun detectCycleInLinkedList(list: LinkedList) : Boolean {
+    val visited = mutableListOf(list)
+    var current = list
+    while(current.next != null) {
+        if (current.next in visited) {
+            return true
+        }
+        current = current.next!!
+    }
     return false
 }
